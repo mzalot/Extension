cc42836232dd0906d425a15e17fc3de3832f61ca
mzalot <69329263+mzalot@users.noreply.github.com>
2022-01-13 10:20:50 -0700

    I removed many files becuase I screwed up my code

--- popup.js
+++ popup.js
@@ -1,9 +1,4 @@
-requirejs(["helper/util"], function(util) {
-    //This function is called when scripts/helper/util.js is loaded.
-    //If util.js calls define(), then this function is not fired until
-    //util's dependencies have loaded, and the util argument will hold
-    //the module value for "helper/util".
-});
+
 var AlphaVantageAPI = require('alpha-vantage-cli').AlphaVantageAPI;
 
 var yourApiKey = 'TQDLS0XBBDSP3ABF';
